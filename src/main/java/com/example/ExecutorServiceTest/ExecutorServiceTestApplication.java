e3210af6beae1d8344bae1a98ef50a804ecee81f
Ashutosh Pandey <ashutoshpandey@INKACTXM023.local>
2018-03-28 20:25:51 +0530

    First Commit Spring boot setup

---
+++ src/main/java/com/example/ExecutorServiceTest/ExecutorServiceTestApplication.java
@@ -0,0 +1,12 @@
+package com.example.ExecutorServiceTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExecutorServiceTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExecutorServiceTestApplication.class, args);
+	}
+}
